789763b4677de7d8db0a34ffd6e4499ae436f59a
ThanhVC <thanhvc.198@gmail.com>
2022-12-22 14:27:56 +0700

    firebase

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: 'https://sunflower-itss.herokuapp.com/',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
